5801b553d370d6795c65dce354df7eedfee49f2c
Mustapha <mustaphayusuf2216@gmail.com>
2024-08-04 21:48:25 +0100

    Initial commit with frontend and backend setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+// server.js file 
+
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
