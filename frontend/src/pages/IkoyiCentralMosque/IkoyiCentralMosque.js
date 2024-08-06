de00f0ba5c56b2a1073c04957b5e6b8fc99695da
Mustapha <mustaphayusuf2216@gmail.com>
2024-08-06 16:22:51 +0100

    added links

--- frontend/src/pages/IkoyiCentralMosque/IkoyiCentralMosque.js
+++ frontend/src/pages/IkoyiCentralMosque/IkoyiCentralMosque.js
@@ -118,8 +118,8 @@ const IkoyiCentralMosque = () => {
         <div className="container mx-auto text-center">
           <p>&copy; 2024 Bureau for Islamic Education. All rights reserved.</p>
           <div className="flex justify-center space-x-4 mt-4">
-            <a href="#" className="text-gray-400 hover:text-white">Privacy Policy</a>
-            <a href="#" className="text-gray-400 hover:text-white">Terms of Service</a>
+            {/* <a href="#" className="text-gray-400 hover:text-white">Privacy Policy</a>
+            <a href="#" className="text-gray-400 hover:text-white">Terms of Service</a> */}
           </div>
         </div>
       </footer>
