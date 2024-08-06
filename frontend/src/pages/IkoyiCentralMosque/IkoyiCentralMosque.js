d3f82d1f7aaf9c974474f9d1a2cf753a84d9f104
Mustapha <mustaphayusuf2216@gmail.com>
2024-08-06 16:29:56 +0100

    routine changes

--- frontend/src/pages/IkoyiCentralMosque/IkoyiCentralMosque.js
+++ frontend/src/pages/IkoyiCentralMosque/IkoyiCentralMosque.js
@@ -12,10 +12,12 @@ const IkoyiCentralMosque = () => {
             <h1 className="text-xl md:text-2xl font-bold text-gray-800">Ikoyi Central Mosque</h1>
           </div>
           <nav className="flex flex-wrap space-x-2 md:space-x-4 mt-2 md:mt-0 w-full md:w-auto justify-center md:justify-end">
-            {/* <a href="/" className="text-gray-800 hover:text-blue-500">Home</a>
-            <a href="/about" className="text-gray-800 hover:text-blue-500">About</a>
-            <a href="/services" className="text-gray-800 hover:text-blue-500">Services</a>
-            <a href="/contact" className="text-gray-800 hover:text-blue-500">Contact</a> */}
+            <Link to="/" className="bg-green-500 text-white px-6 py-2 rounded-full hover:bg-green-600">
+              Home
+            </Link>
+            <Link to="/nuril-islam-school" className="bg-white text-green-500 px-6 py-2 rounded-full hover:bg-gray-100">
+              Nuril Islam School
+            </Link>
           </nav>
         </div>
       </header>
@@ -27,14 +29,6 @@ const IkoyiCentralMosque = () => {
         >
           <div className="text-center bg-black bg-opacity-50 px-4 py-2 rounded">
             <h2 className="text-2xl md:text-4xl font-bold text-white">Welcome to Ikoyi Central Mosque</h2>
-            <div className="mt-4 flex flex-col md:flex-row justify-center space-y-2 md:space-y-0 md:space-x-4">
-              <Link to="/" className="bg-green-500 text-white px-6 py-2 rounded-full hover:bg-green-600">
-                Home
-              </Link>
-              <Link to="/nuril-islam-school" className="bg-white text-green-500 px-6 py-2 rounded-full hover:bg-gray-100">
-                Nuril Islam School
-              </Link>
-            </div>
           </div>
         </section>
 
@@ -69,42 +63,42 @@ const IkoyiCentralMosque = () => {
           <div className="container mx-auto px-4">
             <h2 className="text-2xl md:text-4xl font-bold mb-6 md:mb-12">Contact Information</h2>
             <div className="bg-white p-6 md:p-8 rounded-lg shadow-md">
-              <div className="flex flex-col md:flex-row md:justify-between space-y-8 md:space-y-0 md:space-x-8">
-                <div className="md:w-1/3">
+              <div className="flex flex-col md:flex-row md:justify-between space-y-8 md:space-y-0">
+                <div className="w-full md:w-1/3">
                   <h3 className="text-lg font-semibold">Call Us</h3>
                   <p className="text-gray-700 mt-2">+2347049216166 (Whatsapp/calls)</p>
                 </div>
-                <div className="md:w-1/3">
+                <div className="w-full md:w-1/3">
                   <h3 className="text-lg font-semibold">Write Us</h3>
                   <p className="text-gray-700 mt-2">bureauforislamiceducation@gmail.com</p>
                   <p className="text-gray-700 mt-2">ikoyicentralmosque@gmail.com</p>
                 </div>
-                <div className="md:w-1/3">
+                <div className="w-full md:w-1/3">
                   <h3 className="text-lg font-semibold">Follow Us</h3>
-                  <div className="flex mt-2 space-x-4">
+                  <div className="flex mt-2 space-x-2 md:space-x-4">
                     <a href="https://www.facebook.com/bfieikoyilagos?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                      <i className="fab fa-facebook fa-2x" style={{ color: '#1877F2' }}></i>
+                      <i className="fab fa-facebook fa-lg md:fa-2x" style={{ color: '#1877F2' }}></i>
                     </a>
                     <a href="https://twitter.com/bfieikoyilagos" target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                      <i className="fab fa-twitter fa-2x" style={{ color: '#1DA1F2' }}></i>
+                      <i className="fab fa-twitter fa-lg md:fa-2x" style={{ color: '#1DA1F2' }}></i>
                     </a>
                     <a href="https://t.me/bfieikoyilagos" target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                      <i className="fab fa-telegram fa-2x" style={{ color: '#0088CC' }}></i>
+                      <i className="fab fa-telegram fa-lg md:fa-2x" style={{ color: '#0088CC' }}></i>
                     </a>
                     <a href="https://instagram.com/bfieikoyilagos" target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                      <i className="fab fa-instagram fa-2x" style={{ color: '#C13584' }}></i>
+                      <i className="fab fa-instagram fa-lg md:fa-2x" style={{ color: '#C13584' }}></i>
                     </a>
                     <a href="https://www.youtube.com/@Bfieikoyilagos" target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                      <i className="fab fa-youtube fa-2x" style={{ color: '#FF0000' }}></i>
+                      <i className="fab fa-youtube fa-lg md:fa-2x" style={{ color: '#FF0000' }}></i>
                     </a>
                     <a href="https://www.tiktok.com/@bfieikoyilagos" target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                      <i className="fab fa-tiktok fa-2x" style={{ color: '#000000' }}></i>
+                      <i className="fab fa-tiktok fa-lg md:fa-2x" style={{ color: '#000000' }}></i>
                     </a>
                     <a href="https://www.linkedin.com/in/bureau-for-islamic-education-70213628b?trk=contact-info" target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                      <i className="fab fa-linkedin fa-2x" style={{ color: '#0077B5' }}></i>
+                      <i className="fab fa-linkedin fa-lg md:fa-2x" style={{ color: '#0077B5' }}></i>
                     </a>
                     <a href="http://www.bfie.aaic.org.ng" target="_blank" rel="noopener noreferrer" className="text-blue-600">
-                      <i className="fas fa-globe fa-2x" style={{ color: '#17A2B8' }}></i>
+                      <i className="fas fa-globe fa-lg md:fa-2x" style={{ color: '#17A2B8' }}></i>
                     </a>
                   </div>
                 </div>
