6f5aad0b676ade3a2340612b404974aaea5f56a4
Mustapha <mustaphayusuf2216@gmail.com>
2024-08-08 17:10:53 +0100

    routine updates

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import image1 from '../../assets/bfie1.jpeg';
+import image4 from '../../assets/bfie4.jpeg';
+import image5 from '../../assets/bfie5.jpeg';
+import image6 from '../../assets/bfie6.jpeg';
 import ApprovedDocuments from '../../components/ApprovedDocuments';
 
 const Home = () => {
@@ -47,7 +50,7 @@ const Home = () => {
       <main>
         <section
           className="w-screen h-80 md:h-96 bg-cover bg-center flex items-center justify-center"
-          style={{ backgroundImage: `url(${image1})` }}
+          style={{ backgroundImage: `url(${image4})` }}
         >
           <div className="text-center bg-black bg-opacity-50 px-4 py-2 rounded">
             <h2 className="text-2xl md:text-4xl font-bold text-white">Welcome to Bureau for Islamic Education</h2>
@@ -115,55 +118,58 @@ const Home = () => {
           </div>
         </section>
 
-        <section className="w-screen py-10 md:py-20 bg-gray-50">
-          <div className="container mx-auto text-center">
-            <h2 className="text-2xl md:text-4xl font-bold mb-6 md:mb-12">Prayer Schedule</h2>
-            <div id="scroll-container" className="flex space-x-4 overflow-x-auto">
-              <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
-                <div className="bg-gray-200 p-4 rounded-lg shadow-md">
-                  <h3 className="text-xl font-bold">Dawn Prayer</h3>
-                  <p className="text-2xl text-green-500 font-semibold">Fajr</p>
-                  <p className="text-lg text-gray-700">5:29 am</p>
-                </div>
-              </div>
-              <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
-                <div className="bg-gray-200 p-4 rounded-lg shadow-md">
-                  <h3 className="text-xl font-bold">Sunrise Time</h3>
-                  <p className="text-2xl text-green-500 font-semibold">Duhur</p>
-                  <p className="text-lg text-gray-700">12:52 PM</p>
-                </div>
-              </div>
-              <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
-                <div className="bg-gray-200 p-4 rounded-lg shadow-md">
-                  <h3 className="text-xl font-bold">Afternoon</h3>
-                  <p className="text-2xl text-green-500 font-semibold">Asar</p>
-                  <p className="text-lg text-gray-700">4:10 PM</p>
-                </div>
-              </div>
-              <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
-                <div className="bg-gray-200 p-4 rounded-lg shadow-md">
-                  <h3 className="text-xl font-bold">Sunset Prayer</h3>
-                  <p className="text-2xl text-green-500 font-semibold">Maghrib</p>
-                  <p className="text-lg text-gray-700">9:03 PM</p>
-                </div>
-              </div>
-              <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
-                <div className="bg-gray-200 p-4 rounded-lg shadow-md">
-                  <h3 className="text-xl font-bold">Night Prayer</h3>
-                  <p className="text-2xl text-green-500 font-semibold">Isha</p>
-                  <p className="text-lg text-gray-700">8:11 PM</p>
-                </div>
-              </div>
-              <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
-                <div className="bg-gray-200 p-4 rounded-lg shadow-md">
-                  <h3 className="text-xl font-bold">Sunrise</h3>
-                  <p className="text-2xl text-green-500 font-semibold">Shuruq</p>
-                  <p className="text-lg text-gray-700">2:00 PM</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <section
+  className="w-screen py-10 md:py-20 bg-cover bg-center"
+  style={{ backgroundImage: `url(${image5})` }}
+>
+  <div className="container mx-auto text-center bg-black bg-opacity-50 p-6 rounded-lg">
+    <h2 className="text-2xl md:text-4xl font-bold mb-6 md:mb-12 text-white">Prayer Schedule</h2>
+    <div id="scroll-container" className="flex space-x-4 overflow-x-auto">
+      <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold">Dawn</h3>
+          <p className="text-2xl text-green-500 font-semibold">Fajr</p>
+          <p className="text-lg text-gray-700">5:29 am</p>
+        </div>
+      </div>
+      <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold">Sunrise</h3>
+          <p className="text-2xl text-green-500 font-semibold">Duhur</p>
+          <p className="text-lg text-gray-700">12:52 PM</p>
+        </div>
+      </div>
+      <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold">Afternoon</h3>
+          <p className="text-2xl text-green-500 font-semibold">Asar</p>
+          <p className="text-lg text-gray-700">4:10 PM</p>
+        </div>
+      </div>
+      <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold">Sunset</h3>
+          <p className="text-2xl text-green-500 font-semibold">Maghrib</p>
+          <p className="text-lg text-gray-700">9:03 PM</p>
+        </div>
+      </div>
+      <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold">Night</h3>
+          <p className="text-2xl text-green-500 font-semibold">Isha</p>
+          <p className="text-lg text-gray-700">8:11 PM</p>
+        </div>
+      </div>
+      <div className="p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/6">
+        <div className="bg-gray-200 p-4 rounded-lg shadow-md">
+          <h3 className="text-xl font-bold">Sunrise</h3>
+          <p className="text-2xl text-green-500 font-semibold">Shuruq</p>
+          <p className="text-lg text-gray-700">2:00 PM</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
         <ApprovedDocuments />
 
@@ -177,61 +183,72 @@ const Home = () => {
         </section>
 
         <section className="w-screen py-10 md:py-20 bg-white">
-          <div className="container mx-auto text-center">
-            <h2 className="text-2xl md:text-4xl font-bold mb-6 md:mb-12 text-green-600">Donations and Support</h2>
-            <p className="mt-4 text-base md:text-lg font-semibold mb-6">All Accounts Name: Bureau for Islamic Education</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              <div className="p-6 bg-gray-100 rounded-lg shadow-md">
-                <div className="flex items-center mb-4">
-                  <div className="bg-green-500 text-white p-3 rounded-full mr-4">
-                    <i className="fas fa-cog fa-2x"></i>
-                  </div>
-                  <h3 className="text-xl md:text-2xl font-bold">Maintenance & Operations</h3>
-                </div>
-                <p className="text-gray-700">Union Bank: 0006648320</p>
-                <p className="text-gray-700">Lotus Bank: 1003580434</p>
+  <div className="container mx-auto text-center">
+    <h2 className="text-2xl md:text-4xl font-bold mb-6 md:mb-12 text-green-600">Donations and Support</h2>
+    <p className="mt-4 text-base md:text-lg font-semibold mb-6">All Accounts Name: Bureau for Islamic Education</p>
+    <div className="flex flex-wrap md:flex-nowrap">
+      <div className="w-full md:w-1/2 p-4 flex justify-center">
+        <img src={image6} alt="Donations and Support" className="w-full h-auto rounded-lg shadow-md" />
+      </div>
+      <div className="w-full md:w-1/2 p-4">
+        <div className="grid grid-cols-1 gap-4" style={{ height: '100%' }}>
+          <div className="p-6 bg-gray-100 rounded-lg shadow-md flex flex-col justify-center">
+            <div className="flex items-center mb-4">
+              <div className="bg-green-500 text-white p-3 rounded-full mr-4">
+                <i className="fas fa-cog fa-2x"></i>
               </div>
-              <div className="p-6 bg-gray-100 rounded-lg shadow-md">
-                <div className="flex items-center mb-4">
-                  <div className="bg-green-500 text-white p-3 rounded-full mr-4">
-                    <i className="fas fa-project-diagram fa-2x"></i>
-                  </div>
-                  <h3 className="text-xl md:text-2xl font-bold">Projects</h3>
-                </div>
-                <p className="text-gray-700">Union Bank: 0037173446</p>
-                <p className="text-gray-700">Lotus Bank: 1003580355</p>
-              </div>
-              <div className="p-6 bg-gray-100 rounded-lg shadow-md">
-                <div className="flex items-center mb-4">
-                  <div className="bg-green-500 text-white p-3 rounded-full mr-4">
-                    <i className="fas fa-moon fa-2x"></i>
-                  </div>
-                  <h3 className="text-xl md:text-2xl font-bold">Ramadan Programs / Special Purposes</h3>
-                </div>
-                <p className="text-gray-700">Lotus Bank: 1003580513</p>
-              </div>
-              <div className="p-6 bg-gray-100 rounded-lg shadow-md">
-                <div className="flex items-center mb-4">
-                  <div className="bg-green-500 text-white p-3 rounded-full mr-4">
-                    <i className="fas fa-graduation-cap fa-2x"></i>
-                  </div>
-                  <h3 className="text-xl md:text-2xl font-bold">Scholarship</h3>
-                </div>
-                <p className="text-gray-700">Lotus Bank: 1003580719</p>
-              </div>
-              <div className="p-6 bg-gray-100 rounded-lg shadow-md">
-                <div className="flex items-center mb-4">
-                  <div className="bg-green-500 text-white p-3 rounded-full mr-4">
-                    <i className="fas fa-hand-holding-heart fa-2x"></i>
-                  </div>
-                  <h3 className="text-xl md:text-2xl font-bold">Zakkat / Zakkat-ul-Fitr</h3>
-                </div>
-                <p className="text-gray-700">Lotus Bank: 1003580661</p>
-                <p className="text-gray-700">Union Bank: 0193836571</p>
-              </div>
+              <h3 className="text-xl md:text-2xl font-bold">Maintenance & Operations</h3>
             </div>
+            <p className="text-gray-700">Union Bank: 0006648320</p>
+            <p className="text-gray-700">Lotus Bank: 1003580434</p>
           </div>
-        </section>
+          <div className="p-6 bg-gray-100 rounded-lg shadow-md flex flex-col justify-center">
+            <div className="flex items-center mb-4">
+              <div className="bg-green-500 text-white p-3 rounded-full mr-4">
+                <i className="fas fa-project-diagram fa-2x"></i>
+              </div>
+              <h3 className="text-xl md:text-2xl font-bold">Projects</h3>
+            </div>
+            <p className="text-gray-700">Union Bank: 0037173446</p>
+            <p className="text-gray-700">Lotus Bank: 1003580355</p>
+          </div>
+          <div className="p-6 bg-gray-100 rounded-lg shadow-md flex flex-col justify-center">
+            <div className="flex items-center mb-4">
+              <div className="bg-green-500 text-white p-3 rounded-full mr-4">
+                <i className="fas fa-moon fa-2x"></i>
+              </div>
+              <h3 className="text-xl md:text-2xl font-bold">Ramadan Programs / Special Purposes</h3>
+            </div>
+            <p className="text-gray-700">Lotus Bank: 1003580513</p>
+          </div>
+          <div className="p-6 bg-gray-100 rounded-lg shadow-md flex flex-col justify-center">
+            <div className="flex items-center mb-4">
+              <div className="bg-green-500 text-white p-3 rounded-full mr-4">
+                <i className="fas fa-graduation-cap fa-2x"></i>
+              </div>
+              <h3 className="text-xl md:text-2xl font-bold">Scholarship</h3>
+            </div>
+            <p className="text-gray-700">Lotus Bank: 1003580719</p>
+          </div>
+          <div className="p-6 bg-gray-100 rounded-lg shadow-md flex flex-col justify-center">
+            <div className="flex items-center mb-4">
+              <div className="bg-green-500 text-white p-3 rounded-full mr-4">
+                <i className="fas fa-hand-holding-heart fa-2x"></i>
+              </div>
+              <h3 className="text-xl md:text-2xl font-bold">Zakkat / Zakkat-ul-Fitr</h3>
+            </div>
+            <p className="text-gray-700">Lotus Bank: 1003580661</p>
+            <p className="text-gray-700">Union Bank: 0193836571</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
       </main>
 
       <footer className="bg-gray-800 text-white py-8">
