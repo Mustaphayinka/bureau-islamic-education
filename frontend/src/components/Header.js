73bef2b11dc544cc418636e9df02f17c8f1c20a7
Mustapha <mustaphayusuf2216@gmail.com>
2024-08-20 12:52:09 +0100

    logo change

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import image1 from '../assets/bfie1.jpeg'; // Adjust the path as needed
+import bfielogo from '../assets/bfielogo.jpeg'; // This can be your logo image
 
 const Header = () => {
   return (
     <header className="bg-white border-b border-gray-200 sticky top-0 z-50">
       <div className="container mx-auto flex flex-wrap justify-between items-center py-4 px-4">
         <div className="flex items-center space-x-4">
-          <img src={image1} alt="Logo" className="h-8" />
+          {/* Placeholder for your logo */}
+          <img src={bfielogo} alt="Logo" className="h-8" />
           <h1 className="text-xl md:text-2xl font-bold text-gray-800">Bureau for Islamic Education</h1>
         </div>
         <nav className="flex flex-wrap space-x-2 md:space-x-4 mt-2 md:mt-0 w-full md:w-auto justify-center md:justify-end">
